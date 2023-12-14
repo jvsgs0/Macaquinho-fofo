e59be1a38d0d6042a418b058d22c94127d8b2f87
jvsgs0 <111323425+jvsgs0@users.noreply.github.com>
2023-12-14 17:38:50 -0300

    localdb.js

--- localdb.js
+++ localdb.js
@@ -30,6 +30,6 @@ const db = {
   an: { chunks: ['a', 'n'], phones: ['AE', 'N'] },
   was: { chunks: ['w', 'a', 's'], phones: ['W', 'AA', 'Z'] },
   we: { chunks: ['w', 'e'], phones: ['W', 'IY'] },
-  joão: { chunks: ['j', 'o', 'a', 'o'], phones: ['JH', 'AA', 'OW', 'UW'] }
+  joão: { chunks: ['j', 'o', 'ã', 'o'], phones: ['JH', 'AA', 'OW', 'UW'] }
 };
 export default db;
